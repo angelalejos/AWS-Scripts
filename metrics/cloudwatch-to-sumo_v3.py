517212c692a848bc22c3b3501cba16ff6cf461e4
David Gadoury <gadouryd@gmail.com>
2016-03-01 13:36:51 -0500

    added ability to filter

--- metrics/cloudwatch-to-sumo_v3.py
+++ metrics/cloudwatch-to-sumo_v3.py
@@ -10,21 +10,25 @@ from datetime import datetime, timedelta
 #regions = ["us-east-1", "us-west-1", "us-west-2", "ap-northeast-1", "ap-southeast-1", "ap-southeast-2", "eu-west-1", "sa-east-1"]
 regions = ["us-east-1"]
 
-attributes = ['public_ip_address', 'private_ip_address', 'vpc_id', 'instance_id', 'private_dns_name', 'public_dns_name', 'security_groups', 'instance_type', 'key_name', 'subnet_id']
+# The instances from which you want to collect metrics. Default is all
+filters = []
 
+# Which instance attributes do you want to include with instance metrics
+attributes = ['tags', 'public_ip_address', 'private_ip_address', 'vpc_id', 'instance_id', 'private_dns_name', 'public_dns_name', 'security_groups', 'instance_type', 'key_name', 'subnet_id']
+
+# Which EC2 metrics do you want to capture
 ec2_metrics = [ 'CPUUtilization', 'NetworkIn', 'NetworkOut', 'DiskReadBytes', 'DiskReadOps', 'DiskWriteBytes', 'DiskWriteOps']
-linux_metrics = [ 'DiskSpaceUtilization', 'MemoryUtilization', 'SwapUtilization' ]
 
-#configure logging - http://boto3.readthedocs.org/en/latest/reference/core/boto3.html
-
+# Store results 
 d = {}
 
+# Only required when using write_to_file()
 logfile = '/var/log/cloudwatch_metrics.log'
 
 ### CONSTANTS ###
 # Start and end times for the Cloudwatch query
-query_end = datetime.utcnow()
-query_start = query_end - timedelta(minutes=9)
+QUERY_END = datetime.utcnow()
+QUERY_START = QUERY_END - timedelta(minutes=9)
 
 ### FUNCTIONS ###
 def convert(input):
@@ -51,15 +55,16 @@ def remove_underscore(attrib):
             continue
     new_attrib = "".join(attrib)
     return new_attrib
+
 def write_to_file(logdata):
-    """Writes a given log message to file."""
+    """Write log message to file."""
     fh = open(logfile, "a")
     fh.write(str(logdata) + '\n')
     fh.close()
 
-def push_to_sumo(logdata):
-    """Sends given log message to Hosted Collector"""
-    logdata = str(logdata) + '\n'
+def push_to_collector(logdata):
+    """Sends log message to hosted collector"""
+    logdata = logdata + '\n'
     print urllib2.urlopen(url, logdata).read()
 
 def get_ec2_metrics(metric, instanceId):
@@ -72,8 +77,8 @@ def get_ec2_metrics(metric, instanceId):
                             Namespace='AWS/EC2',
                             MetricName=metric,
                             Dimensions=[{'Name': 'InstanceId', 'Value': instanceId}],
-                            StartTime=query_start,
-                            EndTime=query_end,
+                            StartTime=QUERY_START,
+                            EndTime=QUERY_END,
                             Period=300,
                             Statistics=['Average'])
 
@@ -92,18 +97,25 @@ for region in regions:
     s = boto3.session.Session(region_name=region)
     ec2 = s.resource('ec2')
 
-    for i in ec2.instances.all():
-        d['timestamp'] = str(query_start)
+    for i in ec2.instances.filter(Filters=filters):
+
+        d['timestamp'] = str(QUERY_START)
         d['region'] = region
+
+        id = getattr(i,'id')
+
+        instance = ec2.Instance(id)
+
         for a in range(len(attributes)):
             r = {}
             attrib = list(attributes[a])
-            new_attrib = remove_underscore(attrib)
-            r[attributes[a]] = getattr(i,attributes[a])
+            new_attrib = remove_underscore(attrib) 
+
+            r[attributes[a]] = getattr(instance,attributes[a])
             d[new_attrib] = r[attributes[a]]
 
         for metric in ec2_metrics:
-          get_ec2_metrics(metric, d['instanceId'])
+            get_ec2_metrics(metric, d['instanceId'])
 
+        print d
         write_to_file(d)
-        #push_to_sumo(d)
